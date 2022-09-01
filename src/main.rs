8db1a90cd18d8ae292f12371bb796d1ed81eb3fd
Hanno Braun <hanno@braun-odw.eu>
2022-09-01 19:56:29 +0200

    Print HTML document

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let html = format!("\
+<!DOCTYPE html>
+<html lang=\"en\">
+    <head>
+        <meta charset=\"UTF-8\" />
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />
+
+        <title>Hello, world!</title>
+    </head>
+    <body>
+        <p>Hello, world!</p>
+    </body>
+</html>\
+    ");
+
+    println!("{}", html);
 }
