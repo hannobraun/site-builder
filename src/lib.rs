8690aaeb5b0a96f0b4c3818e324b9ff49c8bb7fd
Hanno Braun <hanno@braun-odw.eu>
2022-09-25 09:50:46 +0200

    Accept `Html` in `render_template`

--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,17 @@ pub mod markdown;
 
 use std::path::Path;
 
+use html::Html;
 use tera::{Context, Tera};
 use tokio::{fs::File, io::AsyncWriteExt};
 
 pub async fn render_template(
     dir: &str,
     name: &str,
-    content: &str,
+    content: &Html,
 ) -> anyhow::Result<String> {
     let mut context = Context::new();
-    context.insert("content", &content);
+    context.insert("content", &content.0);
 
     let tera = Tera::new(dir)?;
     let html = tera.render(name, &context)?;
