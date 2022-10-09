6c033d8824ecf08d58f4c5c20aa2fb7faa0d0ca9
Hanno Braun <hanno@braun-odw.eu>
2022-10-09 10:30:23 +0200

    Add `Template::new`

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,7 @@ use site_builder::{markdown::Markdown, render_template, template::Template};
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let content = Markdown::read("markdown/content.md").await?.parse().await?;
-    let template = Template {
-        directory: "templates/**",
-        name: "base.html",
-    };
+    let template = Template::new("templates/**", "base.html");
     let html = render_template(&template, &content).await?;
     let html_file = html.write_to_file("target/output.html").await?;
     html_file.open_in_browser()?;
--- src/template.rs
+++ src/template.rs
@@ -2,3 +2,9 @@ pub struct Template {
     pub directory: &'static str,
     pub name: &'static str,
 }
+
+impl Template {
+    pub fn new(directory: &'static str, name: &'static str) -> Self {
+        Self { directory, name }
+    }
+}
